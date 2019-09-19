ca79dce5d9bd3d8fa9af139c16e63894a9a10c4f
nangejia <wbn@soutan.com>
2019-09-19 17:29:46 +0800

    更新视频文档

--- golang/02/cmdline.go
+++ golang/02/cmdline.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 )
 
-//传入命令行参数
+//传入命令行参数 单行注释
+
+/*
+这是多行注释
+注释有两种：
+	单行注释
+	多行注释
+ */
 
 func main() {
 	//fmt.Println(os.Args)
 	if len(os.Args) > 1 {
 		fmt.Println("命令行传入参数：", os.Args[1])
 	}
-
 }
